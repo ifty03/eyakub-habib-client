feb8f2c44848f99cef7ca9e314ad4e9d95337051
ifty03 <ashikulislamb84@gmail.com>
2022-06-03 22:57:59 +0600

    site responsive

--- src/Pages/Dashboard/Banner.js
+++ src/Pages/Dashboard/Banner.js
@@ -4,15 +4,42 @@ const Banner = () => {
   return (
     <div
       id="banner"
-      class=" flex flex-wrap items-center p-4 mt-[4100px] md:mt-[2200px] lg:mt-[1680px]  justify-center min-w-full"
+      class=" flex flex-wrap items-center p-4 mt-[5030px] md:mt-[2870px] lg:mt-[2330px]  justify-center min-w-full"
     >
-      <div class="container min-w-full bg-white rounded  shadow-lg transform duration-200 easy-in-out m-12">
-        <div class="lg:h-[400px] sm:h-64  overflow-hidden">
+      <div class="container min-w-full  bg-white rounded  shadow-lg transform duration-200 easy-in-out m-12">
+        <div class="lg:h-[400px] sm:h-64 static overflow-hidden">
           <img
             class="w-[100%] rounded-t"
             src="https://img.freepik.com/free-vector/web-development-coding-programming-futuristic-banner-computer-code-laptop_3482-5572.jpg?w=2000"
             alt="aldi sigun on Unsplash"
           />
+          <div className="absolute hidden md:block top-0 right-0 -mt-16 -mr-2">
+            <div className="flex items-center w-full justify-center py-8">
+              <div className="max-w-sm rounded shadow bg-white dark:bg-gray-800">
+                <div className="flex">
+                  <div className="px-6 py-5">
+                    <p className="text-base font-medium leading-none text-gray-800 dark:text-gray-100">
+                      Congratulations 🎉 Bruce!
+                    </p>
+                    <p className="text-xs leading-3 text-gray-500 dark:text-gray-400 pt-2">
+                      You have won gold medal
+                    </p>
+                    <p className="text-xl font-semibold leading-tight text-pink-600 dark:text-gray-100 pt-6 pt-3">
+                      $ Latest award
+                    </p>
+                    <div className="pt-4">
+                      <button className="py-2 px-4 text-xs font-semibold leading-3 bg-pink-600 rounded hover:bg-pink-600 focus:outline-none text-white">
+                        View details
+                      </button>
+                    </div>
+                  </div>
+                  <div className="px-3">
+                    <img src="https://i.ibb.co/34gPtCT/bg.png" alt="medal" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
 
         <div class="flex justify-start px-5 -mt-12 mb-5">
@@ -59,7 +86,7 @@ const Banner = () => {
             amet consectetur adipisicing elit. Aut quasi veritatis voluptatem
             laborum sapiente dolor odit nobis minima facilis assumenda.
           </p>
-          <button className="btn btn-success btn-sm ml-auto block w-fit mt-4">
+          <button className="btn btn-secondary btn-sm ml-auto block w-fit mt-4">
             Hire Me
           </button>
         </div>
